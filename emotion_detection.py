c0126a0ed6bb31ac07f71aaf0c426bc1606e3cc0
Juan Carlos Arias Botero <jacal2211@gmail.com>
2024-12-01 09:48:45 -0500

    Se crea el __init__ y se cambia el codigo de emotion_detection

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,24 +1,15 @@
 import requests
 
-
 def emotion_detector(variable):
     text_to_analyze = variable
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    headers = {
-        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
-    }
-
-    input_json = {
-        "raw_document": {"text": text_to_analyze}
-    }
-
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyze}}
     response = requests.post(url, headers=headers, json=input_json)
 
     if response.status_code == 200:
         response_data = response.json()
-
-        emociones = response_data['emotionDocument']['emotion']
-
+        emociones = response_data['emotionPredictions'][0]['emotion']
         formato_saliente = {
             "anger": emociones.get("anger", 0),
             "disgust": emociones.get("disgust", 0),
@@ -26,10 +17,8 @@ def emotion_detector(variable):
             "joy": emociones.get("joy", 0),
             "sadness": emociones.get("sadness", 0)
         }
-
         emociones_dominantes = max(formato_saliente, key=formato_saliente.get)
         formato_saliente['emociones_dominantes'] = emociones_dominantes
-
         return formato_saliente
     else:
         return {
