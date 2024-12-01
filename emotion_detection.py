ca2c5cc7ef3c1bdc5fd1f3daf579d7da0aa89126
Juan Carlos Arias Botero <42156547+juanca2918@users.noreply.github.com>
2024-12-01 08:12:38 -0500

    Task 1, 2, 3

---
+++ emotion_detection.py
@@ -0,0 +1,38 @@
+import requests
+
+
+def emotion_detector(variable):
+    text_to_analyze = variable
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    input_json = {
+        "raw_document": {"text": text_to_analyze}
+    }
+
+    response = requests.post(url, headers=headers, json=input_json)
+
+    if response.status_code == 200:
+        response_data = response.json()
+
+        emociones = response_data['emotionDocument']['emotion']
+
+        formato_saliente = {
+            "anger": emociones.get("anger", 0),
+            "disgust": emociones.get("disgust", 0),
+            "fear": emociones.get("fear", 0),
+            "joy": emociones.get("joy", 0),
+            "sadness": emociones.get("sadness", 0)
+        }
+
+        emociones_dominantes = max(formato_saliente, key=formato_saliente.get)
+        formato_saliente['emociones_dominantes'] = emociones_dominantes
+
+        return formato_saliente
+    else:
+        return {
+            "error": "Failed to get a response",
+            "status_code": response.status_code
+        }
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from oaqjp-final-project-emb-ai!")
+
+
+if __name__ == "__main__":
+    main()
