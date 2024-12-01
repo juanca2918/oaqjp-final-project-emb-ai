95ad82c20fe286b726aac4415ed6164ca9bafc79
Juan Carlos Arias Botero <jacal2211@gmail.com>
2024-12-01 10:21:32 -0500

    se crea el archivo server.py

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/emotion_detector', methods=['POST'])
+def emotion_detector_route():
+    statement = request.form.get('statement')
+    result = emotion_detector(statement)
+    
+    if result:
+        response_text = f"For the given statement, the system response is "
+        for emotion, score in result.items():
+            if emotion != 'emociones_dominantes':
+                response_text += f"'{emotion}': {score:.9f}, "
+        response_text = response_text.rstrip(', ')
+        
+        dominant_emotion = result['emociones_dominantes']
+        response_text += f". The dominant emotion is {dominant_emotion}."
+    else:
+        response_text = "Unable to process the statement."
+    
+    return render_template('index.html', response=response_text)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
